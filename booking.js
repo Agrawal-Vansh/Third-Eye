f8be9e17494ba2f66589787a63615280aba5185c
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2024-03-14 23:27:23 +0530

    Booking done

--- booking.js
+++ booking.js
@@ -4,9 +4,24 @@ document.getElementById("rideForm").addEventListener("submit", function(event) {
     var to = document.getElementById("to").value;
     var datetime = document.getElementById("datetime").value;
 
-    // Simulate booking process (can be replaced with actual API call)
-    alert("Ride booked from " + from + " to " + to + " at " + datetime);
-    
-    // Clear form fields
-    document.getElementById("rideForm").reset();
+    // Update map with the new route
+    calculateAndDisplayRoute(from, to);
+
+    // Add the ride to the table
+    addRideToTable(from, to, datetime);
 });
+
+function calculateAndDisplayRoute(start, end) {
+    // Code for calculating and displaying route...
+}
+
+function addRideToTable(from, to, datetime) {
+    var table = document.getElementById("ridesTable").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.rows.length);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    cell1.innerHTML = from;
+    cell2.innerHTML = to;
+    cell3.innerHTML = datetime;
+}
